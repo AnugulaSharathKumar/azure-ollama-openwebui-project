0a1da930c9b42c9f13dceedd059c6fb47a8bc218
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2025-10-16 01:07:23 +0530

    Add multiple outputs for AKS and ACR details
    Added outputs for Kubernetes version, client certificate, client key, cluster CA certificate, Azure Container Registry name, and location.

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,6 +8,11 @@ output "resource_group_name" {
   value       = azurerm_resource_group.main.name
 }
 
+output "kubernetes_version" {
+  description = "Kubernetes version being used"
+  value       = azurerm_kubernetes_cluster.main.kubernetes_version
+}
+
 output "kube_config" {
   description = "Kubernetes config file"
   value       = azurerm_kubernetes_cluster.main.kube_config_raw
@@ -17,4 +22,33 @@ output "kube_config" {
 output "host" {
   description = "Kubernetes cluster host"
   value       = azurerm_kubernetes_cluster.main.kube_config.0.host
+  sensitive   = true
+}
+
+output "client_certificate" {
+  description = "Client certificate"
+  value       = azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
+  sensitive   = true
+}
+
+output "client_key" {
+  description = "Client key"
+  value       = azurerm_kubernetes_cluster.main.kube_config.0.client_key
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  description = "Cluster CA certificate"
+  value       = azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate
+  sensitive   = true
+}
+
+output "acr_name" {
+  description = "Azure Container Registry name"
+  value       = azurerm_container_registry.acr.name
+}
+
+output "location" {
+  description = "Azure region"
+  value       = azurerm_resource_group.main.location
 }
