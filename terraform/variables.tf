cc122f17f572614e6da37fcdbd85c991ae1aca33
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2025-10-15 18:24:14 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,41 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
-  default     = "openwebui-rg"
+  type        = string
+  default     = "openwebui-aks-rg"
 }
 
 variable "location" {
   description = "Azure region"
+  type        = string
   default     = "Central India"
+}
+
+variable "cluster_name" {
+  description = "AKS cluster name"
+  type        = string
+  default     = "openwebui-aks-cluster"
+}
+
+variable "dns_prefix" {
+  description = "DNS prefix for AKS"
+  type        = string
+  default     = "openwebuiaks"
+}
+
+variable "admin_username" {
+  description = "Admin username for AKS nodes"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "node_count" {
+  description = "Number of AKS nodes"
+  type        = number
+  default     = 2
+}
+
+variable "vm_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+  default     = "Standard_D4s_v3"
 }
